f4fac5496eb9827ae1d304c3e22501f92e5526c3
Matheus <mfshops01@gmail.com>
2024-08-24 19:05:35 -0300

    operador ternario

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,15 +1,8 @@
 public class ResultadoEscolar {
     
     public static void main(String[] args) {
-        int nota = 7;
-
-        if(nota >= 7)
-        System.out.println("Aprovado");
-        
-        else if (nota >= 5 && nota < 7)
-        System.out.println("Prova de recuperação");
-
-        else
-        System.out.println("Reprovado");
+        int nota = 4;
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
     }
 }
